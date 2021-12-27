0c2798312a8fb70b4e4b72209360e7a7ce1e7fc6
Amit Kumar Mishra <32227450+agentmishra@users.noreply.github.com>
2021-12-28 04:50:39 +0530

    Update index.js
    added array for hobby list in li, for the sake of removing it and retaining the serial number

--- index.js
+++ index.js
@@ -1,11 +1,26 @@
+let hobby = [];
+let serialNumber = 0;
 function addHobby() {
     
     if(document.getElementById('hobby-input').value ===''){
         alert("Please input a Hobby");
     }
     else{
-        document.getElementById('hobby-list').innerHTML += "<li>" + document.getElementById('hobby-input').value +"</li>";
-        document.getElementById('hobby-input').value = ''
+        hobby.push(document.getElementById('hobby-input').value);
+        document.getElementById('hobby-list').innerHTML ='';
+        let i=0;
+        while(i<hobby.length){
+            serialNumber=i+1;
+            document.getElementById('hobby-list').innerHTML += "<li onclick='this.remove(); removeHobby(this.id);' id="+serialNumber+">" + serialNumber +" - "+ hobby[i] +" <span><i class='fa fa-trash-o'></i></li></span>";
+            i++;
+        }
+        document.getElementById('hobby-input').value = '';
         
     }
-  }
+}
+
+function removeHobby(id) {
+    let removeHobbyNumber = id-1;
+    hobby.splice(removeHobbyNumber, 1, '');
+    
+}
