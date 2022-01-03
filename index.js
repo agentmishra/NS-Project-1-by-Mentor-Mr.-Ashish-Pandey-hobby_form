e2b33038abe5b2c0e6ea7df5dd999529b3eb59ff
Amit Kumar Mishra <32227450+agentmishra@users.noreply.github.com>
2022-01-03 12:17:56 +0530

    used proper functions and implemented edit

--- index.js
+++ index.js
@@ -1,43 +1,68 @@
-let hobby = [];
-let serialNumber = 0;
+const hobbyInput = document.getElementById('hobby-input');
+const hobbyList = document.getElementById('hobby-list');
+const hobby = [];
+
+
 function addHobby() {
-    
-    if(document.getElementById('hobby-input').value ===''){
+    if(hobbyInput.value ===''){
         alert("Please input a Hobby");
     }
     else{
-        hobby.push(document.getElementById('hobby-input').value);
-        document.getElementById('hobby-list').innerHTML ='';
-        let i=0;
-        while(i<hobby.length){
-            if(hobby[i]===''){
-                i++;
-            }
-            else{
-                serialNumber=i+1;
-                document.getElementById('hobby-list').innerHTML += "<li>" + serialNumber +" - "+ hobby[i] +" <span onclick='this.remove(); removeHobby(this.id);' id="+serialNumber+"><i class='fa fa-trash-o'></i></span></li>";
-                i++;
-            }
-        }
-        document.getElementById('hobby-input').value = '';
-        
+        hobby.push(hobbyInput.value);
+        displayLi();
+        clearInput();
     }
 }
 
-function removeHobby(id) {
-    let removeHobbyNumber = id-1;
+function removeHobby(elem) {
+    let removeHobbyNumber = elem.parentNode.id-1;
     hobby.splice(removeHobbyNumber, 1, '');
-    document.getElementById('hobby-list').innerHTML ='';
-        let i=0;
-        while(i<hobby.length){
-            if(hobby[i]===''){
-                i++;
-            }
-            else{
-                serialNumber=i+1;
-                document.getElementById('hobby-list').innerHTML += "<li>" + serialNumber +" - "+ hobby[i] +" <span onclick='this.remove(); removeHobby(this.id);' id="+serialNumber+"><i class='fa fa-trash-o'></i></span></li>";
-                i++;
-            }
+    displayLi();
+}
+
+function displayLi() {
+    let serialNumber = 0;
+    hobbyList.innerHTML ='';
+    let i=0;
+    while(i<hobby.length){
+        if(hobby[i]===''){
+            i++;
         }
-    
+        else{
+            serialNumber=i+1;
+            hobbyList.innerHTML += "<li id=li"+serialNumber+"><p  id=pLabel"+serialNumber+">" + serialNumber +"<label> - "+ hobby[i] +" <button onclick='myToggle("+serialNumber+")'><i class='fa fa-pencil-square-o'> </i></button> <button onclick='removeHobby(this)'><i class='fa fa-trash-o'></i></button></p><p  id=pInput"+serialNumber+" style='display:none;'>" + serialNumber +" - <input type='text' placeholder='Enter the Edited Value' value="+hobby[i]+" id=input"+serialNumber+"> <button onclick='editThis("+serialNumber+")'><i class='fa fa-check'> </i></button> <button onclick='myToggle("+serialNumber+")'><i class='fa fa-close'></i></button></li>";
+            i++;
+        }
+    }
+}
+
+function editThis(liId) {
+    let inputId='input'+liId;
+    let id=liId-1;
+    let edited = document.getElementById(inputId)
+    if(edited.value ===''){
+        alert("Please input a Hobby");
+    }
+    else{
+        hobby[id] = edited.value;
+        displayLi();
+    }
+}
+
+function myToggle(id) {
+    let labelId='pLabel'+id;
+    let inputId='pInput'+id;
+    let x = document.getElementById(labelId);
+    let y = document.getElementById(inputId);
+    if (x.style.display === "none" && y.style.display === "block") {
+      x.style.display = "block";
+       y.style.display = "none";
+    } else {
+      x.style.display = "none";
+      y.style.display = "block";
+    } 
+  }
+
+function clearInput() {
+    hobbyInput.value = '';
 }
